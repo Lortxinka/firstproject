fe4b430dcd9ded823eac26aeae5dfbdedb7d601d
david cvv <davidkadav4@gmail.com>
2023-01-14 15:38:44 +0300

    aaaaaaaaaaaa

---
+++ oped.py
@@ -0,0 +1,52 @@
+print("Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha
+Turi ip ip ip
+Ip ip ip ip tsha ik
+Ip tura ip ik
+Eugh eugh isha ik turisha")
